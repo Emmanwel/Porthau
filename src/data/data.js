98ccea617644ae1e7b10bdc800c3608dda08f5bd
Mahjong5 <emmanuelmukhebi5@gmail.com>
2023-01-10 09:59:01 +0300

    data

--- src/data/data.js
+++ src/data/data.js
@@ -1,24 +1,32 @@
 import WorkIm from '../assets/projects/rooms.PNG'
 import RealEst from '../assets/projects/Leaplugs.PNG'
 import Todo from '../assets/projects/todo.PNG'
+import Portfolio from '../assets/projects/portfolio.PNG'
 
 export const data = [
   {
     id: 1,
+    name: 'Personal Portfolio',
+    image: Portfolio,
+    github: 'https://github.com/Emmanwel/Porthau.git',
+    live: 'https://emmanuelmukhebiportfolio.vercel.app/',
+  },
+  {
+    id: 2,
     name: 'Resorts Reservation Application',
     image: WorkIm,
     github: 'https://github.com/Emmanwel/reservation.git',
     live: 'https://reserveyourrooms.vercel.app/',
   },
   {
-    id: 2,
+    id: 3,
     name: 'Leaplug MVP Application',
     image: RealEst,
     github: '',
     live: 'https://mvp-leaplug.netlify.app/',
   },
   {
-    id: 3,
+    id: 4,
     name: 'Todo Application',
     image: Todo,
     github: 'https://github.com/Emmanwel/Todos.git',
